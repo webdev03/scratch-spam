d3666df7933dc30ce63a5ea9dcec3acf78582bf3
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-01-17 21:57:56 +1300

    I dont like tabs so im reformatting

--- auth.js
+++ auth.js
@@ -5,69 +5,69 @@ const fetch = require("cross-fetch");
  * Run Scratch
  */
 class ScratchSession {
-	async init(user, pass) {
-		this.username = user;
-		// a lot of this code is taken from
-		// https://github.com/CubeyTheCube/scratchclient/blob/main/scratchclient/ScratchSession.py
-		// thanks!
-		const headers = {
-			"x-csrftoken": "a",
-			"x-requested-with": "XMLHttpRequest",
-			Cookie: "scratchcsrftoken=a;scratchlanguage=en;",
-			referer: "https://scratch.mit.edu",
-			"user-agent": "Mozilla/5.0 ScratchSpamProtection",
-		};
-		const loginReq = await fetch("https://scratch.mit.edu/login/", {
-			method: "POST",
-			body: JSON.stringify({
-				username: user,
-				password: pass,
-			}),
-			headers: headers,
-		});
-		if (!loginReq.ok) {
-			throw new Error("Login failed.");
-		}
-		this._headersLogin = loginReq.headers;
+  async init(user, pass) {
+    this.username = user;
+    // a lot of this code is taken from
+    // https://github.com/CubeyTheCube/scratchclient/blob/main/scratchclient/ScratchSession.py
+    // thanks!
+    const headers = {
+      "x-csrftoken": "a",
+      "x-requested-with": "XMLHttpRequest",
+      Cookie: "scratchcsrftoken=a;scratchlanguage=en;",
+      referer: "https://scratch.mit.edu",
+      "user-agent": "Mozilla/5.0 ScratchSpamProtection",
+    };
+    const loginReq = await fetch("https://scratch.mit.edu/login/", {
+      method: "POST",
+      body: JSON.stringify({
+        username: user,
+        password: pass,
+      }),
+      headers: headers,
+    });
+    if (!loginReq.ok) {
+      throw new Error("Login failed.");
+    }
+    this._headersLogin = loginReq.headers;
 
-		// Awesome regexes by ScratchClient - https://github.com/CubeyTheCube/scratchclient/blob/main/scratchclient/ScratchSession.py
-		this.csrfToken = /scratchcsrftoken=(.*?);/gm.exec(
-			loginReq.headers.get("set-cookie")
-		)[1];
-		this.token = /"(.*)"/gm.exec(loginReq.headers.get("set-cookie"))[1];
-		// taken from scratchclient
-		this.cookieSet =
-			"scratchcsrftoken=" +
-			this.csrfToken +
-			";scratchlanguage=en;scratchsessionsid=" +
-			this.token +
-			";";
-	}
-	async deleteComment(id) {
-		const delFetch = await fetch(
-			"https://scratch.mit.edu/site-api/comments/user/god286/del/",
-			{
-				method: "POST",
-				body: JSON.stringify({
-					id: id.toString(),
-				}),
-				headers: {
-					"x-csrftoken": this.csrfToken,
-					"X-Token": this.token,
-					"x-requested-with": "XMLHttpRequest",
-					Cookie: this.cookieSet,
+    // Awesome regexes by ScratchClient - https://github.com/CubeyTheCube/scratchclient/blob/main/scratchclient/ScratchSession.py
+    this.csrfToken = /scratchcsrftoken=(.*?);/gm.exec(
+      loginReq.headers.get("set-cookie")
+    )[1];
+    this.token = /"(.*)"/gm.exec(loginReq.headers.get("set-cookie"))[1];
+    // taken from scratchclient
+    this.cookieSet =
+      "scratchcsrftoken=" +
+      this.csrfToken +
+      ";scratchlanguage=en;scratchsessionsid=" +
+      this.token +
+      ";";
+  }
+  async deleteComment(id) {
+    const delFetch = await fetch(
+      "https://scratch.mit.edu/site-api/comments/user/god286/del/",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          id: id.toString(),
+        }),
+        headers: {
+          "x-csrftoken": this.csrfToken,
+          "X-Token": this.token,
+          "x-requested-with": "XMLHttpRequest",
+          Cookie: this.cookieSet,
 
-					referer: "https://scratch.mit.edu",
-					"user-agent": "Mozilla/5.0 ScratchSpamProtection",
-				},
-			}
-		);
-		if (!delFetch.ok) {
-			console.log(delFetch.status, await delFetch.text());
-			throw new Error("Error deleting comment.");
-		}
-		return delFetch.status;
-	}
+          referer: "https://scratch.mit.edu",
+          "user-agent": "Mozilla/5.0 ScratchSpamProtection",
+        },
+      }
+    );
+    if (!delFetch.ok) {
+      console.log(delFetch.status, await delFetch.text());
+      throw new Error("Error deleting comment.");
+    }
+    return delFetch.status;
+  }
 }
 
 module.exports.ScratchSession = ScratchSession;
